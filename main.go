8b3f561e27cd0e6943d7150abef24a279d92d708
Jonathan Monette <jon.monette@gmail.com>
2020-05-11 22:56:35 -0500

    remove useless logging

--- main.go
+++ main.go
@@ -2,38 +2,15 @@ package main
 
 import (
 	"flag"
-	"log"
-	"os"
 
 	"github.com/jmoney8080/aws-cost-maintenance/cost"
 )
 
 var (
-	// Info Logger
-	Info *log.Logger
-	// Warning Logger
-	Warning *log.Logger
-	// Error Logger
-	Error *log.Logger
-
 	policy string
 )
 
 func init() {
-
-	// A habbit of mine.  I like to have a nice log line when needed.
-	Info = log.New(os.Stdout,
-		"[INFO]: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Warning = log.New(os.Stderr,
-		"[WARNING]: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Error = log.New(os.Stderr,
-		"[ERROR]: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
 	flag.StringVar(&policy, "policy", "", "The policy to run: ")
 }
 
